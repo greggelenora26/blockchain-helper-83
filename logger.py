5666f09b9778600f7873e9944e9ecf5821cae444
Gregg Elenora <greggec22101993@hotmail.com>
2026-09-10 00:06:37 +0200

    cryptographic chained formatter with rotating log handler

--- logger.py
+++ logger.py
@@ -1,30 +1,40 @@
+import hashlib
 import logging
-import os
 from logging.handlers import RotatingFileHandler
 
-def get_blockchain_logger(name='bc_node', log_file='blockchain.log'):
-    logger = logging.getLogger(name)
+class BlockchainFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None):
+        super().__init__(fmt, datefmt)
+        self.prev_hash = "0" * 64
+
+    def format(self, record):
+        original_msg = super().format(record)
+        data_to_hash = f"{self.prev_hash}|{record.created}|{original_msg}"
+        current_hash = hashlib.sha256(data_to_hash.encode('utf-8')).hexdigest()
+        self.prev_hash = current_hash
+        return f"[{current_hash[:16]}] {original_msg}"
+
+def setup_logger(log_file='blockchain.log'):
+    logger = logging.getLogger('blockchain_helper')
     logger.setLevel(logging.DEBUG)
     
-    formatter = logging.Formatter(
-        '%(asctime)s | %(levelname)-8s | %(process)d | %(message)s'
-    )
-    
-    file_handler = RotatingFileHandler(
-        log_file, 
-        maxBytes=10*1024*1024, 
-        backupCount=5
-    )
-    file_handler.setFormatter(formatter)
-    
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    
     if not logger.handlers:
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-    
+        handler = RotatingFileHandler(log_file, maxBytes=1048576, backupCount=3)
+        formatter = BlockchainFormatter(
+            fmt='%(asctime)s | %(levelname)s | %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
+        console = logging.StreamHandler()
+        console.setFormatter(formatter)
+        logger.addHandler(console)
+        
     return logger
 
-# Dynamic initialization for blockchain-helper-83
-chain_logger = get_blockchain_logger()
+if __name__ == '__main__':
+    log = setup_logger()
+    log.info('Genesis block initiated.')
+    log.warning('Transaction pool high latency detected.')
+    log.error('Failed to propagate block #482910.')
