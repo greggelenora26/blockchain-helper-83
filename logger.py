d20aabd391bb23317902411c55fdebf709b7ea0e
Gregg Elenora <greggec22101993@hotmail.com>
2026-09-12 00:07:35 +0200

    rotating file logger initialization for blockchain

--- logger.py
+++ logger.py
@@ -1,29 +1,26 @@
-import hashlib
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
-class BlockchainFormatter(logging.Formatter):
-    def __init__(self, fmt=None, datefmt=None):
-        super().__init__(fmt, datefmt)
-        self.prev_hash = "0" * 64
-
-    def format(self, record):
-        original_msg = super().format(record)
-        data_to_hash = f"{self.prev_hash}|{record.created}|{original_msg}"
-        current_hash = hashlib.sha256(data_to_hash.encode('utf-8')).hexdigest()
-        self.prev_hash = current_hash
-        return f"[{current_hash[:16]}] {original_msg}"
-
-def setup_logger(log_file='blockchain.log'):
-    logger = logging.getLogger('blockchain_helper')
+def get_blockchain_logger(name: str = 'crypto_logger') -> logging.Logger:
+    logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     
     if not logger.handlers:
-        handler = RotatingFileHandler(log_file, maxBytes=1048576, backupCount=3)
-        formatter = BlockchainFormatter(
-            fmt='%(asctime)s | %(levelname)s | %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
+        formatter = logging.Formatter(
+            '%(asctime)s | %(levelname)8s | [TXN_ID:%(process)d] | %(message)s'
         )
+        
+        log_dir = 'logs'
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+            
+        handler = RotatingFileHandler(
+            f'{log_dir}/blockchain.log',
+            maxBytes=5 * 1024 * 1024,
+            backupCount=3
+        )
+        
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         
@@ -32,9 +29,3 @@ def setup_logger(log_file='blockchain.log'):
         logger.addHandler(console)
         
     return logger
-
-if __name__ == '__main__':
-    log = setup_logger()
-    log.info('Genesis block initiated.')
-    log.warning('Transaction pool high latency detected.')
-    log.error('Failed to propagate block #482910.')
