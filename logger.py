9d706b04d2aaeee73bff230171b38a1ae2cecbcd
Gregg Elenora <greggec22101993@hotmail.com>
2026-09-13 00:23:36 +0200

    add rotational logging setup for blockchain-helper-83

--- logger.py
+++ logger.py
@@ -1,31 +1,31 @@
 import logging
 from logging.handlers import RotatingFileHandler
-import os
+import sys
 
-def get_blockchain_logger(name: str = 'crypto_logger') -> logging.Logger:
+def get_blockchain_logger(name: str, log_file: str = 'node.log') -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     
+    formatter = logging.Formatter(
+        '[%(asctime)s] [%(levelname)s] [%(name)s] :: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    file_handler = RotatingFileHandler(
+        log_file, 
+        maxBytes=10 * 1024 * 1024, 
+        backupCount=5
+    )
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+
     if not logger.handlers:
-        formatter = logging.Formatter(
-            '%(asctime)s | %(levelname)8s | [TXN_ID:%(process)d] | %(message)s'
-        )
-        
-        log_dir = 'logs'
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-            
-        handler = RotatingFileHandler(
-            f'{log_dir}/blockchain.log',
-            maxBytes=5 * 1024 * 1024,
-            backupCount=3
-        )
-        
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        
-        console = logging.StreamHandler()
-        console.setFormatter(formatter)
-        logger.addHandler(console)
-        
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+    
     return logger
+
+# Instantiate core logger for blockchain-helper-83
+chain_log = get_blockchain_logger('block-node')
